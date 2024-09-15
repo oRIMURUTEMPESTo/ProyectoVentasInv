91fc8cd5f3fe9ec6aa73ad14403193bed5970395
oRIMURUTEMPESTo <gitcarlos2024@gmail.com>
2024-09-15 13:20:07 -0400

    correcciones y seed productos v2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,9 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            
+            UsuarioSeeder::class,
+            CategoriasSeeder::class,
         ]);
-
         \App\Models\Producto::factory(100)->create();
     }
 }
