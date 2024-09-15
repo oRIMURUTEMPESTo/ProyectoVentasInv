1ae6db430f50048396a3b96338e76f4b551fe3a6
Luiscc445 <144395986+Luiscc445@users.noreply.github.com>
2024-09-15 11:00:27 -0400

    Update 2024_09_15_025051_create_facturas_table.php

--- database/migrations/2024_09_15_025051_create_facturas_table.php
+++ database/migrations/2024_09_15_025051_create_facturas_table.php
@@ -7,18 +7,26 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Ejecuta las migraciones.
      */
     public function up(): void
     {
         Schema::create('facturas', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->integer('id_usuario');
+            $table->integer('id_producto');
+            $table->integer('id_cliente');
+            $table->timestamp('fecha')->useCurrent();
+            $table->decimal('total', 10, 2)->nullable(false);
+            $table->string('estado', 50);
+            $table->foreign('id_usuario')->references('id')->on('usuarios');
+            $table->foreign('id_producto')->references('id')->on('productos');
+            $table->foreign('id_cliente')->references('id')->on('clientes');
         });
     }
 
     /**
-     * Reverse the migrations.
+     * Revierte las migraciones.
      */
     public function down(): void
     {
